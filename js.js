4fd690dc3c9f09f1dfda4da349c94402d662d198
Igor-Lysak <lisakigor485@gmail.com>
2023-03-09 20:31:48 +0200

    third

--- js.js
+++ js.js
@@ -39,6 +39,7 @@ closeBtn.addEventListener("click", function() {
     closeBtn.parentElement.style.display = "none";
 });
 
+// Year
 yearBtn.addEventListener("click", function(){
     if(yearInput.value % 4 == 0){
         yearResult.style.color = "green";
@@ -53,7 +54,9 @@ yearBtn.addEventListener("click", function(){
         yearResult.style.color = "red";
     }
 });
+// /Year
 
+// RandomNumber
 digitBtn.addEventListener("click", function(){
     let randNumber = Math.floor(Math.random() * 10);
     if(randNumber == digitInput.value){
@@ -69,5 +72,209 @@ digitBtn.addEventListener("click", function(){
         digitResult.style.color = "red";
     }
 });
+// /RandomNumber
+
+// MaxNumber
+const inputMaxFirst = document.getElementById("max-number-first-input");
+const inputMaxSecond = document.getElementById("max-number-second-input");
+const inputMaxThird = document.getElementById("max-number-third-input");
+const MaxNumberText = document.getElementById("max-number");
+const MaxNumberBtn = document.getElementById("check-btn");
+
+MaxNumberBtn.addEventListener("click", function(){
+if(inputMaxFirst.value > inputMaxSecond.value){
+    MaxNumberText.innerHTML = inputMaxFirst.value;
+}
+if (inputMaxSecond.value > inputMaxFirst.value){
+    MaxNumberText.innerHTML = inputMaxSecond.value;
+}
+if (inputMaxFirst.value > inputMaxThird.value){
+    MaxNumberText.innerHTML = inputMaxFirst.value;
+}
+if (inputMaxThird.value > inputMaxFirst.value){
+    MaxNumberText.innerHTML = inputMaxThird.value;
+}
+if (inputMaxSecond.value > inputMaxThird.value){
+    MaxNumberText.innerHTML = inputMaxSecond.value;
+}
+if (inputMaxThird.value > inputMaxSecond.value){
+    MaxNumberText.innerHTML = inputMaxThird.value;
+}
+// if (inputMaxThird.value == inputMaxSecond.value){
+//     MaxNumberText.innerHTML = inputMaxThird.value || inputMaxSecond;
+// }
+// if (inputMaxThird.value == inputMaxFirst.value){
+//     MaxNumberText.innerHTML = inputMaxThird.value || inputMaxSecond;
+// }
+
+if (inputMaxThird.value == Text || inputMaxSecond.value == Text || inputMaxFirst.value == Text){
+    MaxNumberText.innerHTML = "Введіть число";
+}
+});
+
+// /MaxNumber
+
+// Slider
+
+let offset = 0;
+const sliderLine = document.querySelector('.my-pictures-section');
+
+document.querySelector('#arrow-right').addEventListener('click', function(){
+    offset = offset + 256;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('#arrow-left').addEventListener('click', function () {
+    offset = offset - 256;
+    if (offset < 0) {
+        offset = 768;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+// /Slider
+
+// Rock Paper Scissors Game
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const rpsBtn = document.getElementById("rps-btn");
+const compPoints = document.getElementById("comp-points-value");
+const playerPoints = document.getElementById("player-points-value");
+const ResultText = document.getElementById("rps-result");
+
+let player;
+
+rock.addEventListener("click", function(){
+    player = "Rock";
+    console.log(player);
+});
+
+paper.addEventListener("click", function(){
+    player = "Paper";
+    console.log(player);
+});
+
+scissors.addEventListener("click", function(){
+    player = "Scissors";
+    console.log(player);
+});
+
+function computerChoice(){
+    var computerChoices = Math.floor(Math.random() * 3);
+    if (computerChoices == 0){
+        computerChoices = "Rock";
+        console.log(computerChoices);
+    }
+    else if(computerChoices == 1){
+        computerChoices = "Paper";
+        console.log(computerChoices);
+    }
+    else if(computerChoices == 2){
+        computerChoices = "Scissors";
+        console.log(computerChoices);
+    }
+    if(player == "Rock" && computerChoices == "Paper"){
+        compPoints.ariaValueText++;
+        compPoints.innerHTML = compPoints.ariaValueText;
+        ResultText.innerText = "Ви програли";
+        ResultText.style.color = "red"
+    }
+    else if(computerChoices == "Rock" && player == "Paper"){
+        playerPoints.ariaValueText++;
+        playerPoints.innerHTML = playerPoints.ariaValueText;
+        ResultText.innerText = "Ви виграли!";
+        ResultText.style.color = "green"
+    }
+    else if(player == "Paper" && computerChoices == "Scissors"){
+        compPoints.ariaValueText++;
+        compPoints.innerHTML = compPoints.ariaValueText;
+        ResultText.innerText = "Ви програли";
+        ResultText.style.color = "red"
+    }
+    else if(computerChoices == "Paper" && player == "Scissors"){
+        playerPoints.ariaValueText++;
+        playerPoints.innerHTML = playerPoints.ariaValueText;
+        ResultText.innerText = "Ви виграли!";
+        ResultText.style.color = "green"
+    }
+    else if(player == "Rock" && computerChoices == "Scissors"){
+        playerPoints.ariaValueText++;
+        playerPoints.innerHTML = playerPoints.ariaValueText;
+        ResultText.innerText = "Ви виграли!";
+        ResultText.style.color = "green"
+    }
+    else if(computerChoices == "Rock" && player == "Scissors"){
+        compPoints.ariaValueText++;
+        compPoints.innerHTML = compPoints.ariaValueText;
+        ResultText.innerText = "Ви програли";
+        ResultText.style.color = "red"
+    }
+    else if(computerChoices == player){
+        ResultText.innerText = "Нічия!";
+        ResultText.style.color = "grey"
+    }
+}
+
+rpsBtn.addEventListener("click", computerChoice);
+// /Rock Paper Scissors Game
+
+// Calculator
+
+const firstDigitInput = document.getElementById("digit-first-input");
+const secondDigitInput = document.getElementById("digit-second-input");
+const resultDigitInput = document.getElementById("digit-result-input");
+// const equalBtn = document.getElementById("equal");
+const PlusBtn = document.getElementById("plus");
+const SubstractBtn = document.getElementById("substract");
+const MultiplyBtn = document.getElementById("multiply");
+const DivisionBtn = document.getElementById("division");
+
+let firstInputValue = firstDigitInput.value;
+let secondInputValue = secondDigitInput.value;
+
+function plus(){
+        firstInputValue = +firstDigitInput.value;
+        secondInputValue = +secondDigitInput.value;
+        resultDigitInput.value = firstInputValue + secondInputValue;
+        console.log(resultDigitInput.value);
+}
+
+function substract(){
+    firstInputValue = +firstDigitInput.value;
+    secondInputValue = +secondDigitInput.value;
+    resultDigitInput.value = firstInputValue - secondInputValue;
+    console.log(resultDigitInput.value);
+}
+
+function multiply(){
+    firstInputValue = +firstDigitInput.value;
+    secondInputValue = +secondDigitInput.value;
+    resultDigitInput.value = firstInputValue * secondInputValue;
+    console.log(resultDigitInput.value);
+}
+
+function division(){
+    firstInputValue = +firstDigitInput.value;
+    secondInputValue = +secondDigitInput.value;
+    resultDigitInput.value = firstInputValue / secondInputValue;
+    console.log(resultDigitInput.value);
+}
+
+DivisionBtn.addEventListener("click", division);
+MultiplyBtn.addEventListener("click", multiply);
+SubstractBtn.addEventListener("click", substract);
+PlusBtn.addEventListener("click", plus);
+// equalBtn.addEventListener("click", equal);
+
+
+// /Calculator
+
+
+
+
 
 
